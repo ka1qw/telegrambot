74a3ad72becb6b54699c4621eb5a2bdebccde1eb
ka1qw <voxy435@gmail.com>
2022-05-25 11:31:02 +0300

    Убрал вывод id в консоль в эхо-хендлере

--- handlers/other.py
+++ handlers/other.py
@@ -4,7 +4,7 @@ from create_bot import bot
 
 async def echo_send(message: types.Message):
     await bot.send_message(message.from_user.id, message.text)
-    print(message.from_user.id)
+    # print(message.from_user.id)
 
 def register_handlers_other(dp: Dispatcher):
     dp.register_message_handler(echo_send)
