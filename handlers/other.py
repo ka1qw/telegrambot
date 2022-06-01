63dbc0b734d6a0f060266808f7041d1b363d95a6
ka1qw <voxy435@gmail.com>
2022-06-01 10:13:53 +0300

    Поменял эхо-хендлер

--- handlers/other.py
+++ handlers/other.py
@@ -3,8 +3,9 @@ from create_bot import bot
 
 
 async def echo_send(message: types.Message):
-    await bot.send_message(message.from_user.id, message.text)
+    await bot.send_message(message.from_user.id, "Неизвестная команда")
     # print(message.from_user.id)
+
 
 def register_handlers_other(dp: Dispatcher):
     dp.register_message_handler(echo_send)
