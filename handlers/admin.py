2f9c7c94248c10a6cb2f0cb49dee5fdd3e4f0272
ka1qw <voxy435@gmail.com>
2022-07-22 10:51:51 +0300

    Добавил функцию обновления клавиатуры для юзеров в админку

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,8 +2,10 @@ from create_bot import bot
 from aiogram import types, Dispatcher
 from phrases.admin_phrases import *
 from keyboards.admin_kbs import *
+from phrases.common_phrases import *
 from aiogram.dispatcher.filters import Text
 from dicts.admins import admin_ids
+from keyboards.main_menu_kbs import mainMenu_kb
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
@@ -16,5 +18,32 @@ async def command_admin_menu(message: types.Message):
         # await bot.send_message(message.from_user.id,message.from_user.id)
 
 
+'''
+Команда для выдачи всем пользователям клавиатуры основного меню.
+Используется после перезапуска бота.
+'''
+async def command_refresh(message: types.Message):
+    admins = []
+    # здесь должен быть список всех пользователей, сейчас только админы - для теста
+    with open("admins.txt", 'r') as user_data:
+        for line in user_data:
+            admins.append(line.replace('\n', ''))
+    count = 1
+    print(
+        f'########################################################################\n'
+        f'                      Начинаю обновление для {len(admins)} юзеров.\n')
+    for user in admins:
+        try:
+            await bot.send_message(user, start_phrase,reply_markup=mainMenu_kb)
+            print(f'[{count}/{len(admins)}] Сообщение пользователю с id = {user} успешно отправлено')
+        except Exception as ex:
+            print(f'[{count}/{len(admins)} ERROR] Ошибка у пользователя с id = {user}: {ex}')
+        finally:
+            count += 1
+    print('\n                           Обновление закончено\n'
+          '########################################################################')
+
+
 def register_handlers_admin(dp: Dispatcher):
-    dp.register_message_handler(command_admin_menu, Text(equals='/admin', ignore_case=True))
+    dp.register_message_handler(command_admin_menu, Text(equals='/admin', ignore_case=True), state='*')
+    dp.register_message_handler(command_refresh, Text(equals='/refresh', ignore_case=True), state='*')
